2162606a7c4a1776d2472eacca6603865a71d4ec
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-01-26 19:27:26 -0600

    Update c.py
    Now opens each city, printing the title of the first item on the first page

--- c.py
+++ c.py
@@ -1,17 +1,29 @@
 import os
 import time
 from selenium import webdriver
-
+from bs4 import *
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# both lists subject to change, but must be the actual craigslist title
 Tcities = {'sanantonio', 'austin', 'houston', 'lubbock', 'dallas', 'waco'}
 Rcities = {'detroit', 'chicago', 'stlouis',
            'memphis', 'baltimore', 'milwaukee'}
 
 for c in Tcities:
     curl = 'https://' + c + '.craigslist.org'
+
     driver.get(curl)
-    link = driver.find_element_by_link_text('auto parts')
-    link.click()
-    time.sleep(4)
+    element = driver.find_element(By.LINK_TEXT, 'auto parts')
+    element.click()
+
+    time.sleep(10)
+
+    e = driver.find_element(
+        By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a')
+
+    html = e.get_attribute('innerText')
+    print(html)
+    time.sleep(5)
