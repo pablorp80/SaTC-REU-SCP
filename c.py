bb7266e0e082337e7058fbc09c8313c70b17f349
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-01-29 02:27:00 -0600

    Update c.py
    more robust solution that utilizes the 'next' button

--- c.py
+++ c.py
@@ -1,4 +1,3 @@
-
 import time
 from selenium import webdriver
 from bs4 import *
@@ -11,27 +10,35 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 time.sleep(5)
-
 # both lists subject to change, but must be the actual craigslist title
 Tcities = {'sanantonio', 'austin', 'houston', 'lubbock', 'dallas', 'waco'}
 Rcities = {'detroit', 'chicago', 'stlouis',
            'memphis', 'baltimore', 'milwaukee'}
 
 # number of items, just for testing purposes
-count = 0
+count = 1
 
 for c in Tcities:
     curl = 'https://' + c + '.craigslist.org'
-    b = False
     driver.get(curl)
 
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.LINK_TEXT, 'auto parts')))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, 'auto parts')))
     element = driver.find_element(By.LINK_TEXT, 'auto parts')
     time.sleep(5)
     element.click()
 
     time.sleep(10)
+
+    x = driver.find_elements(By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a')
+
+    # case for a page that isn't full, want it to leave asap
+    if (x == []):
+        print('nothing found')
+        # otherwise extract the only element
+    else:
+        e = x[0]
+
+    e.click()
 
     prev_link = ""
     cur_link = driver.current_url
@@ -39,65 +46,23 @@ for c in Tcities:
     # simple solution to check whether the next page arrow is working,
     # after clicking the button, if the link doesn't change, go to next city
     while (cur_link != prev_link):
-        if (b == True):
-            break
         # row, skips last row?
-        for i in range(1, 25):
-            if (b == True):
-                break
-            # column
-            for k in range(1, 6):
-                
-                # find the element on the webpage
-                x = driver.find_elements(
-                    By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[%s]/li[%s]/div/a' % (k, i))
-                time.sleep(1)
 
-                # case for a page that isn't full, want it to leave asap
-                if (x == []):
-                    print('nothing found')
-                    b = True
-                    break
-                # otherwise extract the only element
-                else:
-                    e = x[0]
+        # wait until next element is visible
+        WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
 
-                # wait until next element is visible
-                WebDriverWait(driver, 1).until(EC.visibility_of_element_located(
-                    (By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[%s]/li[%s]/div/a' % (k, i))))
+        # move to next element
+        ActionChains(driver).move_to_element(driver.find_element(By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
 
-                # move to next element
-                ActionChains(driver).move_to_element(driver.find_element(
-                    By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[%s]/li[%s]/div/a' % (k, i)))
+        # wait until element is clickable
+        ele = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
 
-                # wait until element is clickable
-                ele = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((
-                    By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[%s]/li[%s]/div/a' % (k, i))))
-
-                # click on element
-                driver.execute_script("arguments[0].click();", ele)
-
-                # make sure item exists before clicking
-                time.sleep(1)
-                driver.back()
-                time.sleep(1)
-                print(count)
-                count = count + 1
-
-        # move to arrow
-        ActionChains(driver).move_to_element(driver.find_element(
-            By.XPATH, '//*[@id="search-toolbars-1"]/div[2]/button[3]'))
-
-        # wait until forward arrow is visible, then find it
-        arrow = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(
-            (By.XPATH, '//*[@id="search-toolbars-1"]/div[2]/button[3]')))
-
-        # set previous link and go to the next page
+        # click on element
         prev_link = cur_link
-        driver.execute_script("arguments[0].click();", arrow)
-        # loading entire page requires more time
-        time.sleep(10)
-        cur_link = driver.current_url
+        driver.execute_script("arguments[0].click();", ele)
 
-    print('all done')
-    time.sleep(5)
+        # make sure item exists before clicking
+        time.sleep(1)
+        print(count)
+        count = count + 1
+        cur_link = driver.current_url
