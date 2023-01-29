095501d4f2bfb75810fe15abc252b3efa18986de
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-01-29 13:35:57 -0600

    Update c.py
    Prints some data, iterates quickly, but doesn't work for all cities. I split the cities into two sets, one for cities that work and one for cities that don't work. This is because some cities have different layouts, so the buttons xpaths are different. If you run this for too long, you will get temporarily blocked from Craigslist!

--- c.py
+++ c.py
@@ -1,3 +1,4 @@
+
 import time
 from selenium import webdriver
 from bs4 import *
@@ -11,58 +12,81 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 time.sleep(5)
 # both lists subject to change, but must be the actual craigslist title
-Tcities = {'sanantonio', 'austin', 'houston', 'lubbock', 'dallas', 'waco'}
-Rcities = {'detroit', 'chicago', 'stlouis',
-           'memphis', 'baltimore', 'milwaukee'}
+
+#working cities, program works for this layout
+wcities = {'sanantonio', 'austin', 'houston', 'lubbock', 'dallas', 'waco', 'newyork'}
+
+#non-working cities, have a different layout
+nwcities = {'detroit', 'chicago', 'stlouis','memphis', 'baltimore', 'milwaukee'}
+
+item1 = '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a'
 
 # number of items, just for testing purposes
 count = 1
 
-for c in Tcities:
+for c in wcities:
     curl = 'https://' + c + '.craigslist.org'
     driver.get(curl)
 
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, 'auto parts')))
+    WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.LINK_TEXT, 'auto parts')))
     element = driver.find_element(By.LINK_TEXT, 'auto parts')
     time.sleep(5)
     element.click()
 
     time.sleep(10)
 
-    x = driver.find_elements(By.XPATH, '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a')
+    WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.XPATH, item1)))
 
-    # case for a page that isn't full, want it to leave asap
-    if (x == []):
-        print('nothing found')
-        # otherwise extract the only element
-    else:
-        e = x[0]
+    x = driver.find_element(
+        By.XPATH, item1)
 
-    e.click()
+    driver.execute_script("arguments[0].click();", x)
 
     prev_link = ""
     cur_link = driver.current_url
 
-    # simple solution to check whether the next page arrow is working,
-    # after clicking the button, if the link doesn't change, go to next city
+    # make sure that the link is changing
     while (cur_link != prev_link):
-        # row, skips last row?
 
-        # wait until next element is visible
-        WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
+        # wait until the 'next' button is visible
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+            (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
 
-        # move to next element
-        ActionChains(driver).move_to_element(driver.find_element(By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
+        # move to 'next' button
+        ActionChains(driver).move_to_element(driver.find_element(
+            By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
 
-        # wait until element is clickable
-        ele = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
+        # wait until 'next' button is clickable
+        ele = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
 
         # click on element
         prev_link = cur_link
         driver.execute_script("arguments[0].click();", ele)
 
         # make sure item exists before clicking
-        time.sleep(1)
-        print(count)
+
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+            (By.XPATH, '//*[@id="titletextonly"]')))
+
+        title = driver.find_element(
+            By.XPATH, '//*[@id="titletextonly"]').text
+
+        p = driver.find_elements(
+            By.XPATH, '/html/body/section/section/h1/span/span[2]')
+
+        if (p == []):
+            price = 'unknown'
+        else:
+            price = p[0].text
+            if (price[0] != '$'):
+                price = 'unknown'
+
+        #print data to terminal for now
+        print(count, end=' ')
+        print('{PRICE: ' + price + '} ', end=' ')
+        print('{TITLE: ' + title + '}')
         count = count + 1
         cur_link = driver.current_url
