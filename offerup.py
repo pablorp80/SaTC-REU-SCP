3ba5d26975176021646a1b650f6a7c6519ceea69
MistyKurien <65862729+MistyKurien@users.noreply.github.com>
2023-01-30 10:05:32 -0600

    Update offerup.py

--- offerup.py
+++ offerup.py
@@ -23,29 +23,30 @@ Tcities = {"houston", "dallas", "waco", "lubbock", "austin"}
 
 
 
-#for c in Tcities:
+for c in Tcities:
     # /5 - Vehicles
     # /9 - Autoparts and accessories
- #   curl= "https://offerup.com/explore/sck/tx/" + c + "/5/9"
-  #  driver.get(curl)
-   # time.sleep(5)
+    curl= "https://offerup.com/explore/sck/tx/" + c + "/5/9"
+    driver.get(curl)
+    time.sleep(5)
     
 
 #List<WebElement> allLinks = driver.findElements(By.tagName("a"))
 
-item1 = '//*[@id="__next"]/div[5]/div[2]/div[3]/main/div[3]/div/div[1]/div/a[1]'
-curl = "https://offerup.com/explore/sck/tx/houston/5/9"
-driver.get(curl)
-time.sleep(5)
+    item1 = '//*[@id="__next"]/div[5]/div[2]/div[3]/main/div[3]/div/div[1]/div/a[1]'
+# curl = "https://offerup.com/explore/sck/tx/houston/5/9"
+    driver.get(curl)
+    time.sleep(5)
 
-WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.XPATH, item1)))
+    WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.XPATH, item1)))
 
-x = driver.find_element(By.XPATH, item1)
+    x = driver.find_element(By.XPATH, item1)
 
-element = driver.find_element(By.XPATH, item1)
-print(element.text) # proof that it is reading the first object
-element.click()
-time.sleep(5) # clicked on the first link
+    element = driver.find_element(By.XPATH, item1)
+    print(element.text) # it is reading the first object
+    
+    element.click()
+    time.sleep(5)
 
 
