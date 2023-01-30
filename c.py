cfa3dc5658026e4fb23181fd1846f5b95dd33cde
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-01-30 15:48:18 -0600

    Update c.py
    Fixes bug skipping first item and printing last item twice. Changed the order of reading data and clicking the 'next' button

--- c.py
+++ c.py
@@ -1,7 +1,5 @@
-
 import time
 from selenium import webdriver
-from bs4 import *
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,8 +11,11 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 time.sleep(5)
 # both lists subject to change, but must be the actual craigslist title
 
-# working cities, program works for this layout
-wcities = {'sanantonio', 'austin', 'houston','lubbock', 'dallas', 'waco', 'newyork'}
+# working cities, program works for this layout, works best to put in
+# one city at a time rather than treating it as a true set
+
+#'sanantonio', 'austin', 'houston','lubbock', 'dallas', 'waco', 'newyork'
+wcities = {'lubbock'}
 
 # non-working cities, have a different layout
 nwcities = {'detroit', 'chicago', 'stlouis',
@@ -26,8 +27,6 @@ item1 = '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a'
 count = 1
 
 for c in wcities:
-    # avoid printing last item twice, FIXME quick fix
-    last = False
     curl = 'https://' + c + '.craigslist.org'
     driver.get(curl)
 
@@ -45,6 +44,7 @@ for c in wcities:
     x = driver.find_element(
         By.XPATH, item1)
 
+    # get first element of a city
     driver.execute_script("arguments[0].click();", x)
 
     prev_link = ""
@@ -52,29 +52,6 @@ for c in wcities:
 
     # make sure that the link is changing
     while (cur_link != prev_link):
-
-        # wait until the 'next' button is visible
-        try:
-            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
-                (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
-        # go to next city
-        except:
-            last = True
-            break
-        # move to 'next' button
-        ActionChains(driver).move_to_element(driver.find_element(
-            By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
-
-        # wait until 'next' button is clickable
-        ele = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
-            (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
-
-        # click on element
-        prev_link = cur_link
-        # time.sleep(3)
-        driver.execute_script("arguments[0].click();", ele)
-
-        # make sure item exists before clicking
         try:
             WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="titletextonly"]')))
@@ -96,9 +73,28 @@ for c in wcities:
                 price = 'unknown'
 
         # print data to terminal for now
-        if (not last):
-            print(count, end=' ')
-            print('{PRICE: ' + price + '} ', end=' ')
-            print('{TITLE: ' + title + '}')
-            count = count + 1
-            cur_link = driver.current_url
+        print(count, end=' ')
+        print('{PRICE: ' + price + '}', end=' ')
+        print('{TITLE: ' + title + '}')
+        count = count + 1
+
+        # wait until the 'next' button is visible
+        try:
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+                (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
+        # go to next city
+        except:
+            break
+        # move to 'next' button
+        ActionChains(driver).move_to_element(driver.find_element(
+            By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
+
+        # wait until 'next' button is clickable
+        ele = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
+
+        # click on element
+        prev_link = cur_link
+        # time.sleep(5)
+        driver.execute_script("arguments[0].click();", ele)
+        cur_link = driver.current_url
