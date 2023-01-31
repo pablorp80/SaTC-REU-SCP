74aab12bc7c6bb4615733d87e5e5d9dc402f2ab4
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-01-31 14:16:04 -0600

    Update c.py
    Prints date, id, price, title, region, and description. Still no images, and not sure about usernames yet

--- c.py
+++ c.py
@@ -14,14 +14,14 @@ time.sleep(5)
 # working cities, program works for this layout, works best to put in
 # one city at a time rather than treating it as a true set
 
-#'sanantonio', 'austin', 'houston','lubbock', 'dallas', 'waco', 'newyork'
+#'sanantonio', 'austin', 'houston','lubbock', 'dallas', 'waco',
+# 'newyork', 'losangeles', 'sacramento', 'sfbay'
+
+#copy/paste into the set for testing as needed ^
 wcities = {'lubbock'}
 
 # non-working cities, have a different layout
-nwcities = {'detroit', 'chicago', 'stlouis',
-            'memphis', 'baltimore', 'milwaukee'}
-
-item1 = '//*[@id="search-results-page-1"]/ol/ol/ol[1]/li[1]/div/a'
+#nwcities = {'detroit', 'chicago', 'stlouis','memphis', 'baltimore', 'milwaukee', }
 
 # number of items, just for testing purposes
 count = 1
@@ -33,16 +33,13 @@ for c in wcities:
     WebDriverWait(driver, 30).until(
         EC.visibility_of_element_located((By.LINK_TEXT, 'auto parts')))
     element = driver.find_element(By.LINK_TEXT, 'auto parts')
-    time.sleep(5)
     element.click()
 
-    time.sleep(10)
-
     WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.XPATH, item1)))
+        EC.visibility_of_element_located((By.CLASS_NAME, 'titlestring')))
 
     x = driver.find_element(
-        By.XPATH, item1)
+        By.CLASS_NAME, 'titlestring')
 
     # get first element of a city
     driver.execute_script("arguments[0].click();", x)
@@ -52,16 +49,37 @@ for c in wcities:
 
     # make sure that the link is changing
     while (cur_link != prev_link):
+        # get listing title of item
         try:
             WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="titletextonly"]')))
+            title = driver.find_element(
+                By.XPATH, '//*[@id="titletextonly"]').text
         except:
-            cur_link = driver.current_url
-            continue
+            title = 'unknown'
 
-        title = driver.find_element(
-            By.XPATH, '//*[@id="titletextonly"]').text
+        # get item id
+        try:
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+                (By.XPATH, '/html/body/section/section/section/div[2]/p[1]')))
+            id = driver.find_element(
+                By.XPATH, '/html/body/section/section/section/div[2]/p[1]').text
+            id = id.replace('post id: ', '')
+        except:
+            title = 'unknown'
 
+        try:
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+                (By.XPATH, '/html/body/section/section/section/div[2]/p[2]/time')))
+            date = driver.find_element(
+                By.XPATH, '/html/body/section/section/section/div[2]/p[2]/time')
+            driver.execute_script("arguments[0].click();", date)
+
+            date = date.text
+        except:
+            date = 'unknown'
+
+        # get price of item
         p = driver.find_elements(
             By.XPATH, '/html/body/section/section/h1/span/span[2]')
 
@@ -71,20 +89,39 @@ for c in wcities:
             price = p[0].text
             if (price[0] != '$'):
                 price = 'unknown'
+        # get the description of the item
+        try:
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+                (By.ID, 'postingbody')))
+            d = driver.find_element(
+                By.ID, 'postingbody').text
+            # 'show contact info' can be added to a description
+            # as a button so a buyer can contact the seller. The name
+            # of this button, however, is interpreted as a string and
+            # should be ignored in this case
+            d = d.replace('show contact info', '')
+        except:
+            d = 'unknown'
 
         # print data to terminal for now
-        print(count, end=' ')
-        print('{PRICE: ' + price + '}', end=' ')
-        print('{TITLE: ' + title + '}')
+        print(count)
+        print('{DATE:   ' + date + '}')
+        print('{ID:     ' + id + '}')
+        print('{PRICE:  ' + price + '}')
+        print('{TITLE:  ' + title + '}')
+        print('{REGION: ' + c + '}')
+        print('{DESCRIPTION: \n' + d + '}')
+        print('\n\n')
         count = count + 1
 
-        # wait until the 'next' button is visible
+        # navigate to the 'next' button
         try:
             WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
                 (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
-        # go to next city
+        # program is over for this city
         except:
             break
+
         # move to 'next' button
         ActionChains(driver).move_to_element(driver.find_element(
             By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]'))
@@ -93,8 +130,9 @@ for c in wcities:
         ele = WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
             (By.XPATH, '/html/body/section/section/header/div[1]/div/a[3]')))
 
-        # click on element
+        # click on 'next button' and store previous url
         prev_link = cur_link
-        # time.sleep(5)
+        #sleeping for 1 second has been enough to not get flagged
+        time.sleep(1)
         driver.execute_script("arguments[0].click();", ele)
         cur_link = driver.current_url
