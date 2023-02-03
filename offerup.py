f9ae8d477c961d9c701842fc454678e68cabc4e6
MistyKurien <65862729+MistyKurien@users.noreply.github.com>
2023-02-02 22:09:39 -0600

    Update offerup.py
    now prints
    Name ( + price + location)
    Description
    Product ID

--- offerup.py
+++ offerup.py
@@ -31,17 +31,12 @@ for c in Tcities:
 
     print ("In "+ c + " right now!")
 
+    
     curl= "https://offerup.com/explore/sck/tx/" + c + "/5/9"
     driver.get(curl)
     time.sleep(1)
     
-
-#List<WebElement> allLinks = driver.findElements(By.tagName("a"))
-
     genericPath = '//*[@id="__next"]/div[5]/div[2]/div[3]/main/div[3]/div/div[1]/div/a['
-
-    
-# curl = "https://offerup.com/explore/sck/tx/houston/5/9"
 
     
     time.sleep(1)
@@ -49,24 +44,50 @@ for c in Tcities:
     while (True):
         item1 = genericPath + str(a) + "]"
         try:
-            WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+            WebDriverWait(driver, 60).until(EC.visibility_of_element_located(
                 (By.XPATH, item1)))
         # go to next city
         except:
             last = True
             break
         
-        #while(WebDriverWait(driver, 30).until(
-         #   EC.visibility_of_element_located((By.XPATH, item1)))):
-
+        #CLICK ON ITEM
         x = driver.find_element(By.XPATH, item1)
-
         element = driver.find_element(By.XPATH, item1)
         print ("ITERATION " + str(a))
-        #print(element.text) 
-        # # prints the name, price, location
+        
+        #PRINT NAME, PRICE, LOCATION
         print(WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, item1))).get_attribute("aria-label"))
         element.click()
+        time.sleep(1)
+        
+        #PRINTING DESCRIPTION
+        descPath = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[3]/div[2]/div[2]/div/p'
+        descWithDetails = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[5]/div[2]/div[2]/div/p'
+        print("Description: ")
+        try:
+            #Prints this if "Details exist"
+            print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descWithDetails))).text)
+        except:
+            #Checking if "Description" exists
+            try:
+                #Prints Description
+                print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descPath))).text)
+            except:
+                try:
+                    #Prints this if Description has a scroll bar
+                    descPath2 = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[5]/div[2]/div[2]/div[1]/div/div/div/p'
+                    print((WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descPath2))).text))
+                except:
+                    print("No description")
+
+        #GET PRODUCT ID
+        url = driver.current_url
+        parts = url.split("/")
+        productID = parts[-1]
+        print("Product ID: " + productID)
+
+        print("\n ********* \n")
         time.sleep(1)
         a += 1 # increment anchor
         item1 = genericPath + str(a) + "]"
