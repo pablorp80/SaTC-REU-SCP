6a9025ac8806fe05a55d2d97e31cbb62b72ebede
MistyKurien <65862729+MistyKurien@users.noreply.github.com>
2023-02-05 07:46:56 -0600

    Update offerup.py
    prints description, name, price, region, product id, username.
    
    issues:
    no date, description sometimes does not print if there is a scroll

--- offerup.py
+++ offerup.py
@@ -1,3 +1,7 @@
+#fixme : TO DO NOW:
+# catch if there is no description at all. 
+# something wrong with scroll descriptions. maybe make it the first test case.
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
@@ -20,10 +24,12 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 #reads san antonio city is covered under houston
 Tcities = {"houston", "dallas", "waco", "lubbock", "austin"}
+CaliCities = {"san_francisco", "oakland", "east_los_angeles", "bakersfield", "san_jose", "san_diego", "sacramento"}
+curlRoot = "https://offerup.com/explore/sck/ca/"
 
 
 
-for c in Tcities:
+for c in CaliCities:
     # /5 - Vehicles
     # /9 - Autoparts and accessories
 
@@ -32,7 +38,7 @@ for c in Tcities:
     print ("In "+ c + " right now!")
 
     
-    curl= "https://offerup.com/explore/sck/tx/" + c + "/5/9"
+    curl= curlRoot + c + "/5/9"
     driver.get(curl)
     time.sleep(1)
     
@@ -57,35 +63,54 @@ for c in Tcities:
         print ("ITERATION " + str(a))
         
         #PRINT NAME, PRICE, LOCATION
-        print(WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, item1))).get_attribute("aria-label"))
-        element.click()
-        time.sleep(1)
+        #print(WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, item1))).get_attribute("aria-label"))
+        #element.click()
+        #time.sleep(1)
+        itemText = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, item1))).get_attribute("aria-label")
         
+        
+        element.click() 
+
         #PRINTING DESCRIPTION
         descPath = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[3]/div[2]/div[2]/div/p'
         descWithDetails = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[5]/div[2]/div[2]/div/p'
         print("Description: ")
         try:
             #Prints this if "Details exist"
-            print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descWithDetails))).text)
+            print(WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, descWithDetails))).text)
         except:
             #Checking if "Description" exists
             try:
                 #Prints Description
-                print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descPath))).text)
+                print(WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, descPath))).text)
             except:
                 try:
                     #Prints this if Description has a scroll bar
                     descPath2 = '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[2]/div/div[5]/div[2]/div[2]/div[1]/div/div/div/p'
-                    print((WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, descPath2))).text))
+                    print((WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, descPath2))).text))
                 except:
                     print("No description")
+
+        #GET USER NAME
+        name = (driver.find_element(By.XPATH, '//*[@id="__next"]/div[5]/div[2]/main/div[1]/div/div[1]/div/div[5]/button/div/div[2]/p[1]').text)
+        print("Username: " + name)
 
         #GET PRODUCT ID
         url = driver.current_url
         parts = url.split("/")
         productID = parts[-1]
         print("Product ID: " + productID)
+
+        #DETAILS
+        parts = itemText.split("$")
+        name = parts[0].strip()
+        parts = parts[1].split("in")
+        price = parts[0].strip()
+        region = parts[1].strip()
+
+        print("Item name: " + name)
+        print("Item price: " + price)
+        print("Region: " +  region)
 
         print("\n ********* \n")
         time.sleep(1)
