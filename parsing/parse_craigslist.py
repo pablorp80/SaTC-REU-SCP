af77973507c4ee7d7199bf352ca6fe3956944030
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-01-31 13:13:31 -0600

    Add functionality to see which images are still posted

--- parsing/parse_craigslist.py
+++ parsing/parse_craigslist.py
@@ -1,12 +1,33 @@
 import csv
+import requests
 
 # format: [id, date, city, ti_n, title, po_n, post, pi_n, pictures, price]
-def get_data():
+def get_image_urls():
     filepath = 'real.csv'
-    titles = [] # format: [id, title]
+    urls = [] # format: [id, title]
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         next(reader, None) # skip headers
         for row in reader:
-            titles.append((row[0], row[4])) # id and title
-    return titles
+            url_list = row[8].split(',')
+            for url in url_list:
+                urls.append(url.strip())
+    return urls
+
+def check_images(image_urls):
+    success, fail = 0,0
+    for url in image_urls:
+        try:
+            requests.get(url)
+            success += 1
+        except:
+            fail += 1
+    print("Success: " + str(success))
+    print("Fail: " + str(fail))
+
+def main():
+    urls = get_image_urls()
+    check_images(urls)
+
+if __name__ == "__main__":
+    main()
