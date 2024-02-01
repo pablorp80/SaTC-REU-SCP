847d5bb43095d672a21eb1a40b648500e9153937
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2024-01-31 19:55:16 -0600

    correct parsing for newer picture dataset format

--- parsing/parse_craigslist.py
+++ parsing/parse_craigslist.py
@@ -1,43 +1,54 @@
 import csv
+import random
 import requests
+import json
 
 # format: [id, date, city, ti_n, title, po_n, post, pi_n, pictures, price]
-def get_image_urls():
-    filepath = 'real.csv'
-    urls = [] # format: [id, title]
+def get_image_file_names():
+    filepath = 'data.csv'
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         next(reader, None) # skip headers
+        id_to_image_names = {}
         for row in reader:
-            url_list = row[8].split(' ')
-            for url in url_list:
-                urls.append(url)
-    urls = [x for x in urls if x != '']
-    return urls
+            id = row[0]
+            images = row[8]
+            processed_data_str = images.replace('""', '"')[1:-1]
+            data_map = json.loads('{' + processed_data_str + '}')
+            file_names = list(data_map.values())
+            id_to_image_names[id] = file_names
+
+    return id_to_image_names
+
+
 def check_images(image_urls):
     success, fail = 0, 0
+    count = 0
     for url in image_urls:
+        count += 1
+        print(count)
         try:
             response = requests.get(url)
-            # Check if the Content-Type header indicates an image
             if 'image' in response.headers.get('Content-Type', ''):
                 success += 1
             else:
-                print(url)
                 fail += 1
         except:
             fail += 1
     print("Success: " + str(success))
     print("Fail: " + str(fail))
 
-    with open('results.txt', 'a') as file:
+    with open('results2.txt', 'a') as file:
         file.write(f"{success}\n")
         file.write(f"{fail}\n")
 
 def main():
-    urls = get_image_urls()
-    batch = urls[:100]
-    check_images(batch)
+    id_to_images = get_image_file_names()
+    post_id, images = random.choice(list(id_to_images.items()))
+    print(post_id + '\n')
+    for image in images:
+        print(image)
+
 
 if __name__ == "__main__":
     main()
