588aa60dd5516ea8e5faa2de97d2e85c0c4f842d
andrewhamara <andrew_hamara1@baylor.edu>
2024-02-25 23:15:01 -0600

    write to id file simultaneously in case of crash

--- embedding/create_embeddings.py
+++ embedding/create_embeddings.py
@@ -4,57 +4,40 @@ from imagebind.models import imagebind_model
 from imagebind.models.imagebind_model import ModalityType
 import pandas as pd
 import os
-import random
 from parsing.parse_craigslist import get_image_file_names
 
 def create_embedding(model, device, text, images):
 
-    image_embedding_sum = None
-    num_images = len(images)
-    for image in images:
-        image = '.assets/images/' + image
-        input = {
-            ModalityType.VISION : data.load_and_transform_vision_data([image], device)
-        }
-        with torch.no_grad():
-            vision_embedding = model(input)
-        if image_embedding_sum is None:
-            image_embedding_sum = vision_embedding[ModalityType.VISION]
-            print('start')
-            print(image_embedding_sum.shape)
-        else:
-            image_embedding_sum += vision_embedding
-
-    image_embedding_avg = image_embedding_sum / num_images
-
-    text_input = {
-        ModalityType.TEXT : data.load_and_transform_text(text, device)
+#    print('Creating embedding for post:  \n')
+#    print(text + '\n')
+#    for image in images:
+#        print(image)
+    images = ['.assets/images/' + image for image in images]
+    inputs = {
+        ModalityType.VISION : data.load_and_transform_vision_data(images, device),
+        ModalityType.TEXT : data.load_and_transform_text([text], device) # TODO discuss this
     }
-
     with torch.no_grad():
-        embedding = model(text_input)
+        embedding = model(inputs)
 
+    image_embedding = embedding[ModalityType.VISION]
+    image_embedding_avg = torch.mean(image_embedding, dim=0)
     text_embedding = embedding[ModalityType.TEXT]
-    #TODO figure out why this varies
-    print(text_embedding.shape)
 
     combined_embedding_average = (image_embedding_avg + text_embedding) / 2
-    print(combined_embedding_average.shape)
-    print('end')
     return combined_embedding_average
 
-
-def flatten_embedding(embedding):
+def flatten_reshape_embedding(embedding):
     flattened_tensor = embedding.cpu().numpy().flatten()
-    return flattened_tensor
+    reshaped_flattened_tensor = flattened_tensor.reshape(1, -1)
+    return reshaped_flattened_tensor
 
 def write_embedding(flattened_embedding):
     try:
         embeddings_df = pd.DataFrame(flattened_embedding) # create pandas dataframe
-        csv_file_path = 'embeddings.csv'
+        csv_file_path = 'new_embeddings.csv'
         write_header = not os.path.exists(csv_file_path) # don't write header every time...
         embeddings_df.to_csv(csv_file_path, mode='a', index=False, header=write_header)
-        print(f"Embedding written to {csv_file_path}")
         return True
     except:
         print("Error writing embeddings")
@@ -79,23 +62,24 @@ def get_data():
 
 def main():
     posts = get_data()
+    posts = posts[2500:3000]
     model,device = initialize_model()
-    with open('ids.txt', 'r') as f:
+    with open('new_ids.txt', 'r') as f:
         ids = [line.strip() for line in f]
 
-    new_ids = set()
+    posted = 0
     for post in posts:
         curid, text, images = post
         if curid not in ids:
             post_embedding = create_embedding(model, device, text, images)
-            flattened = flatten_embedding(post_embedding)
+            flattened = flatten_reshape_embedding(post_embedding)
             if write_embedding(flattened):
-                new_ids.add(curid)
+                posted += 1
+                print(posted)
+                with open('new_ids.txt', 'a') as f:
+                    f.write(curid + '\n')
         else:
             print('duplicate not written')
-    with open('ids.txt', 'a') as f:
-        for post_id in new_ids:
-            f.write(post_id + '\n')
 
 if __name__ == "__main__":
     main()
