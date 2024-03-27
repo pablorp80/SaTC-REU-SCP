42109b4612cb23c120970539001f518d6062afd3
andrewhamara <andrew_hamara1@baylor.edu>
2024-03-27 18:24:53 -0500

    update parsing script

--- embedding/parsing/parse_craigslist.py
+++ embedding/parsing/parse_craigslist.py
@@ -1,10 +1,23 @@
 import csv
-import random
 import json
+from cleantext import clean
 
-# format: [id, date, city, ti_n, title, po_n, post, pi_n, pictures, price]
+def clean_text(text):
+    return clean(text,
+                 fix_unicode=True,               # fix various unicode errors
+                 to_ascii=True,                  # transliterate to closest ASCII representation
+                 lower=True,                     # lowercase text
+                 no_line_breaks=True,            # fully strip line breaks as opposed to only normalizing them
+                 no_urls=True,                   # replace all URLs with a special token
+                 no_emails=True,                 # replace all email addresses with a special token
+                 no_phone_numbers=True,          # replace all phone numbers with a special token
+                 no_numbers=False,               # do not replace numbers
+                 no_digits=False,                # do not replace digits
+                 no_currency_symbols=True,       # replace all currency symbols with a special token
+                 no_punct=False)                 # don't fully remove punctuation
+
 def get_image_file_names():
-    filepath = 'data.csv'
+    filepath = 'full_data.csv'
     with open(filepath, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         next(reader, None) # skip headers
@@ -12,11 +25,11 @@ def get_image_file_names():
         for row in reader:
             id_title_images = tuple()
             id = row[0]
-            title = row[4]
-            description = row[6]
-            images = row[8]
+            title = clean_text(row[5].strip())
+            description = clean_text(row[7].strip())
+            images = row[9]
 
-            if (title.strip() and description.strip()):
+            if (title and description):
                 text = title + '\n' + description
 
                 processed_data_str = images.replace('""', '"')
@@ -26,15 +39,4 @@ def get_image_file_names():
                 if len(file_names) > 0:
                     id_title_images = id, text, file_names
                     posts.append(id_title_images)
-
     return posts
-
-def main():
-    id_title_imagepaths = get_image_file_names()
-    post_id, title, images = random.choice(id_title_imagepaths)
-    print(post_id + '\n\n' + title + '\n\n')
-    for image in images:
-        print(image)
-
-if __name__ == "__main__":
-    main()
